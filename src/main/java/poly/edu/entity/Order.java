cf00ec3ec436931dca5db6f91bb44d0c3e124bdb
NgMinhHung <nguyenminhhung02082007@gmail.com>
2025-10-17 08:45:14 +0700

    Trang trí lại giao diện trang Thank You

---
+++ src/main/java/poly/edu/entity/Order.java
@@ -0,0 +1,44 @@
+package poly.edu.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(length = 20)
+    private String status;
+
+    @Column(name = "total_amount")
+    private BigDecimal totalAmount;
+
+    @Column(name = "receiver_name")
+    private String receiverName;
+
+    @Column(name = "receiver_phone")
+    private String receiverPhone;
+
+    @Column(name = "receiver_address")
+    private String receiverAddress;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<OrderDetail> details;
+}
